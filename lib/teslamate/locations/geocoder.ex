1a48368f4619025f44a21fd5214a080ed1fd0c3d
Andy Xin <gococonu7@outlook.com>
2025-05-20 22:52:30 +0800

    update proxy domain

--- lib/teslamate/locations/geocoder.ex
+++ lib/teslamate/locations/geocoder.ex
@@ -5,7 +5,7 @@ defmodule TeslaMate.Locations.Geocoder do
 
   adapter Tesla.Adapter.Finch, name: TeslaMate.HTTP, receive_timeout: 30_000
 
-  plug Tesla.Middleware.BaseUrl, "https://nominatim-osm.fusever.com"
+  plug Tesla.Middleware.BaseUrl, "https://nominatim-osm.mytesla.cc"
   plug Tesla.Middleware.Headers, [{"user-agent", "TeslaMate/#{@version}"}]
   plug Tesla.Middleware.JSON
   plug Tesla.Middleware.Logger, debug: true, log_level: &log_level/1
